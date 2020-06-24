fb02ed9a39bfe447e677591dbe74321ac6fbf6ee
Richard O. Gregory <richardgrecoson@gmail.com>
2020-06-24 17:50:13 +0100

    Set up admin router architecture.

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
+
+// For public files
+app.use(express.static("public"))
 // Express should automatically use middleware in requests
 app.use(bodyParser.urlencoded({extended: true}));
 // For storing user session
@@ -11,7 +15,11 @@ app.use(cookieSession({
   keys:["randomStringOfCharacters"]
 }));
 
+// Authentications router
 app.use(authRouter);
+
+// For admin cruding products
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
---
+++ routes/admin/products.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/admin/products", (req, res) => {
+
+});
+
+router.get("/admin/new", (req, res) => {
+
+});
+
+module.exports = router;
